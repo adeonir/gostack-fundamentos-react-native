a3b04a17eb9ed50560c88d23f1e09f2724d8c0e3
Adeonir Kohl <adeonir@gmail.com>
2021-02-01 09:00:08 -0300

    Complete the challenge

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -1,8 +1,12 @@
-import React, { useState, useMemo } from 'react';
+import React, { useMemo } from 'react';
 
 import { useNavigation } from '@react-navigation/native';
 
 import FeatherIcon from 'react-native-vector-icons/Feather';
+
+import formatValue from '../../utils/formatValue';
+
+import { useCart } from '../../hooks/cart';
 import {
   Container,
   CartPricing,
@@ -10,10 +14,6 @@ import {
   CartButtonText,
   CartTotalPrice,
 } from './styles';
-
-import formatValue from '../../utils/formatValue';
-
-import { useCart } from '../../hooks/cart';
 
 // Calculo do total
 // Navegação no clique do TouchableHighlight
@@ -24,15 +24,23 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const total = products.reduce((acc, product) => {
+      const subTotal = product.price * product.quantity;
+
+      return acc + subTotal;
+    }, 0);
+
+    return formatValue(total);
 
     return formatValue(0);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((acc, product) => {
+      return acc + product.quantity;
+    }, 0);
 
-    return 0;
+    return total;
   }, [products]);
 
   return (
